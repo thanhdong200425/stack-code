3331ba6d00f1e366918b8f8ac8f57558af602632
Thanh Dong <dongnt.22ite@vku.udn.vn>
2024-11-22 17:55:24 +0700

    Add loading state to SignUpButton, SignInButton and update related components for better user feedback

--- components/subLayoutComponents/authentication/sub-components/Form.js
+++ components/subLayoutComponents/authentication/sub-components/Form.js
@@ -10,7 +10,7 @@ import Link from "next/link";
 import DividerWithText from "@/components/subLayoutComponents/divider/DividerWithText";
 
 export default function FormContainer({ inputFields = null, children, isHaveLicense = true, isSignIn = false, formAction }) {
-    const [formState, dispatch] = useActionState(formAction, {});
+    const [formState, dispatch, isPending] = useActionState(formAction, {});
 
     return (
         <form action={dispatch} className={classes.formContainer}>
@@ -37,7 +37,7 @@ export default function FormContainer({ inputFields = null, children, isHaveLice
                 </label>
             )}
 
-            <SignUpButtonArea text={isSignIn ? "Log in" : "Create your account"} isSignIn={isSignIn} />
+            <SignUpButtonArea text={isSignIn ? "Log in" : "Create your account"} isSignIn={isSignIn} isPending={isPending} />
 
             <DividerWithText text={"OR"} />
 
@@ -46,32 +46,34 @@ export default function FormContainer({ inputFields = null, children, isHaveLice
     );
 }
 
-export function SignUpButton({ text, iconPath = null, altIconPath = null, widthIcon = null, heightIcon = null, additionClassName = "" }) {
-    const { pending } = useFormStatus();
-
-    const handleClick = (event) => {
-        if (pending) {
-            event.preventDefault();
-        }
-    };
-
+export function SignUpButton({ text, iconPath = null, altIconPath = null, widthIcon = null, heightIcon = null, additionClassName = "", isPending }) {
     return (
         <div className="flex justify-center items-center w-full sm:w-[40%]">
-            <button type="submit" onClick={handleClick} className={`w-full max-w-[250px] py-3 px-2 ${iconPath ? "text-zinc-800 border-[1px] border-black hover:bg-[#155bb5] hover:text-white hover:shadow-lg" : "text-white"} ${!iconPath && "bg-gray-300"} rounded-3xl text-sm cursor-pointer mb-6 hover:bg-[#005bb5] hover:text-[#f0f0f0] text-center flex items-center justify-center gap-2 space-x-2 ${additionClassName}`}>
+            <button type="submit" className={`w-full max-w-[250px] py-3 px-2 ${iconPath ? "text-zinc-800 border-[1px] border-black hover:bg-[#155bb5] hover:text-white hover:shadow-lg" : "text-white"} ${!iconPath && "bg-gray-300"} rounded-3xl text-sm cursor-pointer mb-6 hover:bg-[#005bb5] hover:text-[#f0f0f0] text-center flex items-center justify-center gap-2 space-x-2 ${additionClassName}`}>
                 {iconPath && <Image src={iconPath} alt={altIconPath} width={widthIcon} height={heightIcon} />}
-                <span>{text}</span>
+                <span>
+                    {!isPending ? (
+                        text
+                    ) : (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
+                                <animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12" />
+                            </path>
+                        </svg>
+                    )}
+                </span>
             </button>
         </div>
     );
 }
 
-export function SignUpButtonArea({ isSignIn, text }) {
+export function SignUpButtonArea({ isSignIn, text, isPending }) {
     let subText = isSignIn ? `Don't have account?` : `Already have an account?`;
     let href = isSignIn ? "/sign-up" : "/sign-in";
 
     return (
         <div className="flex justify-center items-center flex-col">
-            <SignUpButton text={text} />
+            <SignUpButton text={text} isPending={isPending} />
             <div className="flex gap-1 leading-4">
                 <p className="font-normal">{subText}</p>
                 <Link href={href} className="text-[#155bb5]">
